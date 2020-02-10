dca790248ebf397bcfd08d047c5b672201b2fa3d
Philipp <phil.black@gmx.net>
2020-02-10 18:28:27 +0100

    added watchdogs if dir is modified then the file will be moved to correct  directory

--- automation/moving_files.py
+++ automation/moving_files.py
@@ -1,21 +1,38 @@
 import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
 
 
-def moveFiles():
-    extensions = {
-        "png": "/Users/Admin/Pictures/Download/png",
-        "jpg": "/Users/Admin/Pictures/Download/jpg",
-        "zip": "/Users/Admin/Documents/zip",
-        "exe": "/Users/Admin/Documents/Anwendungen"
-    }
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        extensions = {
+            "png": "/Users/Admin/Pictures/Download/png",
+            "jpg": "/Users/Admin/Pictures/Download/jpg",
+            "zip": "/Users/Admin/Documents/zip",
+            "exe": "/Users/Admin/Documents/Anwendungen"
+        }
 
-    folder_to_track = "/Users/Admin/Downloads"
-    for filename in os.listdir(folder_to_track):
-        for key in extensions:
-            if filename[-3:] == key:
-                src = "{}/{}".format(folder_to_track, filename)
-                new_destination = "{}/{}".format(extensions[key], filename)
-                os.rename(src, new_destination)
+        for filename in os.listdir(folder_to_track):
+            for key in extensions:
+                if filename[-3:] == key:
+                    src = "{}/{}".format(folder_to_track, filename)
+                    new_destination = "{}/{}".format(extensions[key], filename)
+                    os.rename(src, new_destination)
+                    print(
+                        f"{filename} has been succesfully moved to {extensions[key]}")
 
 
-moveFiles()
+folder_to_track = "/Users/Admin/Downloads"
+event_Handler = MyHandler()
+my_observer = Observer()
+
+my_observer.schedule(event_Handler, folder_to_track, True)
+
+my_observer.start()
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    my_observer.stop()
+    my_observer.join()
