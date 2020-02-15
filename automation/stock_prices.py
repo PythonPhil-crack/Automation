91674abe7862d8ccb6abf0962a504c5e7a24e13a
Philipp <phil.black@gmx.net>
2020-02-15 13:53:48 +0100

    Everything works with selenium

--- automation/stock_prices.py
+++ automation/stock_prices.py
@@ -9,7 +9,6 @@ class Agent:
         self.driver = webdriver.Chrome()
 
     def check_price(self):
-
         self.driver.get(
             "https://www.google.com/search?q=abott+stcok&oq=abott+stcok&aqs=chrome..69i57j0l7.5354j1j8&sourceid=chrome&ie=UTF-8")
         sleep(2)
@@ -17,14 +16,14 @@ class Agent:
         global aktueller_preis
         aktueller_preis = self.driver.find_element_by_xpath(
             "/html/body/div[7]/div[3]/div[9]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/g-card-section[1]/div/g-card-section/span[1]/span/span[1]").text
-        aktueller_preis = float(aktueller_preis[0]+aktueller_preis[1])
+        aktueller_preis = float(aktueller_preis.replace(",", "."))
         print(aktueller_preis)
 
         self.driver.quit()
 
     def send_mail(self):
         self.driver.quit()
-        server = smtplib.SMTP("mail.gmx.net", port=587)
+        server = smtplib.SMTP("smtp.gmail.com", port=587)
         server.ehlo_or_helo_if_needed()
         server.starttls()
         server.ehlo_or_helo_if_needed()
@@ -34,7 +33,7 @@ class Agent:
 
         subject = "Ihre Aktie ist gestiegen!"
 
-        message = f"Betreff: {subject}\n\n Der neue Preis ist {aktueller_preis}$"
+        message = f"Subject:{subject}\n\n Der neue Preis ist {aktueller_preis}€"
         server.sendmail(username, receiver, message)
         server.quit()
 
